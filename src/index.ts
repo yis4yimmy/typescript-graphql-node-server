a26160df80f52a663ea880cd4bf4e3b2d0407d52
Jim Schuster <jim@schusie.net>
2020-03-11 22:28:03 -0700

    Setup basic graphql server

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
 import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { buildSchema, Resolver, Query } from "type-graphql";
 import { createConnection } from "typeorm";
 
-createConnection()
-  .then(_connection => {
-    console.log("Created a connection");
-  })
-  .catch(error => {
-    console.error(error);
+@Resolver()
+class HelloResolver {
+  @Query(() => String)
+  async hello() {
+    return "Hello, world!";
+  }
+}
+
+(async function() {
+  createConnection()
+    .then(_connection => {
+      console.log("Created a connection");
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+  const app = express();
+
+  const schema = await buildSchema({ resolvers: [HelloResolver] });
+
+  const server = new ApolloServer({ schema });
+
+  server.applyMiddleware({ app });
+
+  app.listen({ port: 4000 }, () => {
+    console.log("Server running at http://localhost:4000/graphql");
   });
+})();
