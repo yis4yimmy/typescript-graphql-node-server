1182d9195e4a83351720b2bb1e0f334373594c5f
Jim Schuster <jim@schusie.net>
2020-03-13 11:48:26 -0700

    Add method to create a confirmation url

---
+++ src/authentication/createConfirmEmailUrl.ts
@@ -0,0 +1,23 @@
+import { v4 as generateV4Uuid } from "uuid";
+import { redisClient } from "../services/redis";
+
+interface ConfirmEmailParams {
+  id: string;
+}
+
+export const createConfirmEmailIUrl = async (
+  params: ConfirmEmailParams
+): Promise<string | Error> => {
+  const { HOST_URL, PORT } = process.env;
+  const { id: userId } = params;
+
+  const redisKey = generateV4Uuid();
+
+  const result = await redisClient.set(redisKey, userId, "EX", 60 * 60 * 24);
+
+  if (result !== "OK") {
+    return new Error("Could not create a confirmation email url.");
+  }
+
+  return `${HOST_URL}:${PORT}/authentication/confirmEmail/${redisKey}`;
+};
