6c27ed185ab682e166317090858cb5963fa282bf
Jim Schuster <jim@schusie.net>
2020-03-13 18:02:28 -0700

    Add tests for createConfirmEmailUrl

---
+++ src/authentication/createConfirmEmailUrl.test.ts
@@ -0,0 +1,59 @@
+/* global jest */
+
+import { redisClient } from "../services/redis";
+import { createConfirmEmailIUrl } from "./createConfirmEmailUrl";
+
+describe("createConfirmEmailUrl", () => {
+  const userId = "321cba";
+  let redisClientSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    redisClientSpy = jest.spyOn(redisClient, "set");
+  });
+
+  afterEach(() => {
+    redisClientSpy.mockRestore();
+  });
+
+  describe("failed url creation", () => {
+    let result: string | Error;
+
+    beforeEach(async () => {
+      redisClientSpy.mockImplementationOnce(() => "Error");
+
+      result = await createConfirmEmailIUrl({ id: userId });
+    });
+
+    it("sets the token and userId in Redis with an 86400 second expiry time", () => {
+      expect(redisClientSpy.mock.calls[0][1]).toEqual(userId);
+      expect(redisClientSpy.mock.calls[0][2]).toEqual("EX");
+      expect(redisClientSpy.mock.calls[0][3]).toEqual(60 * 60 * 24);
+    });
+
+    it("returns an error", () => {
+      expect(result).toBeInstanceOf(Error);
+    });
+  });
+
+  describe("successful url creation", () => {
+    let result: string | Error;
+
+    beforeEach(async () => {
+      redisClientSpy.mockImplementationOnce(() => "OK");
+
+      result = await createConfirmEmailIUrl({ id: userId });
+    });
+
+    it("sets the token and userId in Redis with an 86400 second expiry time", () => {
+      expect(redisClientSpy.mock.calls[0][1]).toEqual(userId);
+      expect(redisClientSpy.mock.calls[0][2]).toEqual("EX");
+      expect(redisClientSpy.mock.calls[0][3]).toEqual(60 * 60 * 24);
+    });
+
+    it("returns a url with the confirmation token as a url param", () => {
+      expect(result).toMatch(
+        /http:\/\/localhost:4000\/authentication\/confirmEmail\//
+      );
+    });
+  });
+});
